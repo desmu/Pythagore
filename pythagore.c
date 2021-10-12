fa23fc7eb49e2a390e14b01387808e6ac723e067
desmu <90912716+desmu@users.noreply.github.com>
2021-10-12 21:54:43 +0100

    Update pythagore.c

--- pythagore.c
+++ pythagore.c
@@ -8,29 +8,38 @@
 #endif
 int main(void)
 {
-    int len[2];
-    int bc;
-    char *cote[3]= {"AB","BC"};
-    int count;
+    char *cote[3]= {"AB","BC"},q;
+    int count,on=1,subon=1,len[2],bc;
     do
     {
         bc=1;
         count=0;
+        do
+        {
+            printf("Tracer un triangle [o] ou [n] : ");
+            scanf(" %c",&q);
+            if(q=='o'){
+                subon=0;
+            }else if(q=='n'){
+                on=0;
+                subon=0;
+            }else{
+                printf("\n[o] ou [n] svp\n");
+            }
+        }while(subon==1);
+
+
         for(int i=0; i<2; i++)
         {
-            printf("Cote %s en cm (0 pour sortir): \n",cote[i]);
+            printf("\nCote %s en cm : \n",cote[i]);
             scanf(" %i",&len[i]);
-            if(len[i]==0)
-            {
-                return 0;
-            }
         }
         printf("hypothenuse[CA] = %f\n", sqrt(len[0]*len[0]+len[1]*len[1]));
         for(int i=0; i<len[0]; i++)
         {
             for(int j=0; j<bc; j++)
             {
-                //sleep(1);
+                sleep(1);
                 printf("#");
             }
             printf("\n");
@@ -40,14 +49,23 @@ int main(void)
             }
             else
             {
-                count++;
-                if(count>=(len[0]/len[1])){
-                    bc++;
-                    count=0;
+                if((len[0]/len[1])>=2){
+                    count++;
+                    if(count>=(len[0]/len[1]))
+                    {
+                        bc++;
+                        count=0;
+                    }
+                }else{
+                    count++;
+                    if(count>4*(len[0]/len[1]))
+                    {
+                        bc+=4;
+                        count=0;
+                    }
                 }
             }
         }
     }
-    while(1);
-
+    while(on==1);
 }
