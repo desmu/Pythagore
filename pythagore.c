002152fcca979a35a4beda2ac96f8381290063bf
desmu <90912716+desmu@users.noreply.github.com>
2021-10-14 19:35:39 +0100

    Update pythagore.c

--- pythagore.c
+++ pythagore.c
@@ -8,63 +8,67 @@
 #endif
 int main(void)
 {
-    char *cote[3]= {"AB","BC"},q;
-    int count,on=1,subon=1,len[2],bc;
+    char *cote[2]= {"AB","BC"},q;
+    int on=1,subon=1;
+    float len[2],hor=1;
     do
     {
-        bc=1;
-        count=0;
+        hor=1;
         do
         {
             printf("Tracer un triangle [o] ou [n] : ");
             scanf(" %c",&q);
-            if(q=='o'){
+            if(q=='o')
+            {
                 subon=0;
-            }else if(q=='n'){
+            }
+            else if(q=='n')
+            {
                 on=0;
                 subon=0;
-            }else{
+            }
+            else
+            {
                 printf("\n[o] ou [n] svp\n");
             }
-        }while(subon==1);
+        }
+        while(subon==1);
 
 
         for(int i=0; i<2; i++)
         {
             printf("\nCote %s en cm : \n",cote[i]);
-            scanf(" %i",&len[i]);
+            scanf(" %f",&len[i]);
         }
         printf("hypothenuse[CA] = %f\n", sqrt(len[0]*len[0]+len[1]*len[1]));
-        for(int i=0; i<len[0]; i++)
+        float dist=1;
+        for(float i=0; i<len[0]; i++)
         {
-            for(int j=0; j<bc; j++)
+            if(len[0]>len[1])
             {
-                //sleep(1);
-                printf("#");
-            }
-            printf("\n");
-            if(len[1]>=len[0])
-            {
-                bc+=(len[1]/len[0]);
-            }
-            else
-            {
-                if((len[0]/len[1])>=2){
-                    count++;
-                    if(count>=(len[0]/len[1]))
+                for(float j=0; j<len[1]; j++)
+                {
+
+                    if(j<dist)
                     {
-                        bc++;
-                        count=0;
+                        //sleep(1);
+                        printf("#");
                     }
-                }else{
-                    count++;
-                    if(count>4*(len[0]/len[1]))
+                    else
                     {
-                        bc+=4;
-                        count=0;
+                        printf(" ");
                     }
                 }
+            }else{
+                for(float j=0; j<hor; j++)
+                {
+                    //sleep(1);
+                    printf("#");
+                }
+                hor+=(len[1]/len[0])+(len[1]/(len[0]*len[0]));
             }
+            printf("\n");
+            dist += len[1]/len[0];
         }
     }
     while(on==1);
